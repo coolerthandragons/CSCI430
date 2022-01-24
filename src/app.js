466e8f493f049004959ffaffbacbf787b2378c67
Rachel Cubbage <cubbagerachel123@email.com>
2022-01-24 11:53:48 -0500

    fixed router

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ const path = require('path')
 
 const app = express()
 
-const dir = path.join(__dirname, "../public/index.html")
+const dir = path.join(__dirname, "../public")
 app.use(express.static(dir))
+
 app.get('*', (req,res)=>{
     res.sendFile(path.join(__dirname, "../public/404.html"))
 })
