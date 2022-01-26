137bfe75911a192c0ea087a3f8b2daa3194a13ac
Rachel Cubbage <cubbagerachel123@email.com>
2022-01-25 21:38:04 -0500

    Added Routers

--- src/app.js
+++ src/app.js
@@ -3,12 +3,23 @@ const path = require('path')
 
 const app = express()
 
-const dir = path.join(__dirname, "../public")
-app.use(express.static(dir))
+const indexRouter = require('../routers/index') 
+const aboutMeRouter = require('../routers/aboutMe')
+const createaccountRouter = require('../routers/create-account')
+const loginRouter = require('../routers/login')
+const mainRouter = require('../routers/main') 
+const _404Router = require('../routers/404')
 
-app.get('*', (req,res)=>{
-    res.sendFile(path.join(__dirname, "../public/404.html"))
-})
+
+const dir = path.join(__dirname, "../public") 
+app.use(express.static(dir)) 
+
+app.use(indexRouter) 
+app.use(aboutMeRouter)
+app.use(createaccountRouter)
+app.use(loginRouter)
+app.use(mainRouter)
+app.use(_404Router)
 
 const port = process.env.PORT || 3000
 
